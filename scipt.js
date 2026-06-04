2ce3a64254ceee0fb3d8da88aee7a8aa75b45b05
tOOm-13 <harrytom1303@gmail.com>
2026-06-04 10:13:57 +0200

    4. commit - filtrování, kategorie a vyhledávání + html recepty do js

---
+++ scipt.js
@@ -0,0 +1,175 @@
+const recipes = [
+    {
+        id: 1,
+        title: "Česnečka",
+        category: "Polévky",
+        image: "https://d50-a.sdn.cz/d_50/c_img_QN_2/zZ9CA/cesnecka-polevka-cesnekova-polevka.jpeg?fl=cro,168,0,2664,1998%7Cres,1200,,1%7Cjpg,80,,1"
+    },
+    {
+        id: 2,
+        title: "Perník",
+        category: "Zákusky",
+        image: "https://static.toprecepty.cz/fotky/recepty/0016/nadychany-pernik-38847-670-377-nw.jpg"
+    },
+    {
+        id: 3,
+        title: "Karbenátky",
+        category: "Hlavní jídla",
+        image: "https://static.albert.cz/medias/sys_master/images/h73/ha9/8950413983774.jpg?buildNumber=7224a46dd6e69c0d2357dc801e1625e3358ae6f91d762dca5e4969382ef1cfa5"
+    },
+    {
+        id: 4,
+        title: "Vafle",
+        category: "Snídaně",
+        image: "https://cdn.myshoptet.com/usr/www.madebykristina.cz/user/documents/upload/Recepty/vafle4.jpg"
+    }
+
+];
+
+const container = document.getElementById("recipesContainer");
+
+function createCard(recipe) {
+    return `
+        <div class="col-12 col-md-6 col-lg-3">
+            <div class="card h-100 shadow-sm">
+
+                <img
+                    src="${recipe.image}"
+                    class="card-img-top"
+                    alt="${recipe.title}"
+                >
+
+                <div class="card-body">
+
+                    <h5 class="card-title">
+                        ${recipe.title}
+                    </h5>
+
+                    <p class="card-text">
+                        ${recipe.category}
+                    </p>
+
+                    <a
+                        href="recipe.html?id=${recipe.id}"
+                        class="btn btn-outline-primary"
+                    >
+                        Celý recept
+                    </a>
+
+                </div>
+
+            </div>
+        </div>
+    `;
+}
+
+function renderRecipes(recipesToRender) {
+
+    container.innerHTML = "";
+
+    recipesToRender.forEach(recipe => {
+        container.innerHTML += createCard(recipe);
+    });
+
+}
+
+renderRecipes(recipes);
+
+// Kategorie
+let selectedCategory = "all";
+
+const currentCategory =
+    document.getElementById("currentCategory");
+
+const categoryButtons =
+    document.querySelectorAll(".category-filter");
+
+categoryButtons.forEach(button => {
+
+    button.addEventListener("click", e => {
+
+        e.preventDefault();
+
+        selectedCategory = button.dataset.category;
+
+        currentCategory.textContent =
+            selectedCategory === "all"
+                ? "Všechny recepty"
+                : `${selectedCategory}`;
+
+        categoryButtons.forEach(btn => {
+            btn.classList.remove("active-category");
+        });
+
+        button.classList.add("active-category");
+
+        filterRecipes();
+
+    });
+
+});
+
+// Diakritika
+function normalizeText(text) {
+
+    return text
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+
+}
+
+// Filtrování
+function filterRecipes() {
+
+    const searchValue =
+        normalizeText(searchInput.value);
+
+    const filtered = recipes.filter(recipe => {
+
+        const matchesSearch =
+            normalizeText(recipe.title)
+                .includes(searchValue);
+
+        const matchesCategory =
+            selectedCategory === "all"
+            ||
+            recipe.category === selectedCategory;
+
+        return matchesSearch && matchesCategory;
+
+    });
+
+    renderRecipes(filtered);
+
+}
+
+// Vyhledávání
+const searchInput =
+    document.getElementById("searchInput");
+
+searchInput.addEventListener("input", () => {
+
+    selectedCategory = "all";
+
+    currentCategory.textContent =
+        "Kategorie: Všechny recepty";
+
+    categoryButtons.forEach(btn => {
+        btn.classList.remove("active-category");
+    });
+
+    filterRecipes();
+
+});
+
+
+//Diakritika
+function normalizeText(text) {
+
+    return text
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+
+}
